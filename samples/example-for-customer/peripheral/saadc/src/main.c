543277c117b4657930ebcdb353c145704a435383
jackjinxb <1570698943@qq.com>
2022-03-03 15:17:36 +0800

    make saadc demo

--- samples/example-for-customer/peripheral/saadc/src/main.c
+++ samples/example-for-customer/peripheral/saadc/src/main.c
@@ -7,43 +7,11 @@
 /** @file
  *  @brief Nordic mesh light switch sample
  */
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/mesh/models.h>
-#include <bluetooth/mesh/dk_prov.h>
-#include <dk_buttons_and_leds.h>
-#include "model_handler.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+#include <zephyr.h>
 #include <nrfx_saadc.h>
-
-
-
-static void bt_ready(int err)
-{
-	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
-		return;
-	}
-
-	printk("Bluetooth initialized\n");
-
-	dk_leds_init();
-	dk_buttons_init(NULL);
-
-	err = bt_mesh_init(bt_mesh_dk_prov_init(), model_handler_init());
-	if (err) {
-		printk("Initializing mesh failed (err %d)\n", err);
-		return;
-	}
-
-	if (IS_ENABLED(CONFIG_SETTINGS)) {
-		settings_load();
-	}
-
-	/* This will be a no-op if settings_load() loaded provisioning info */
-	bt_mesh_prov_enable(BT_MESH_PROV_ADV | BT_MESH_PROV_GATT);
-//	bt_mesh_prov_enable(BT_MESH_PROV_ADV);
-	printk("Mesh initialized\n");
-}
 
 static volatile bool is_ready;
 static nrf_saadc_value_t sample;
@@ -89,12 +57,6 @@ void main(void)
 
 	printk("Initializing...\n");
 
-	err = bt_enable(bt_ready);
-	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
-	}
-
-
     while (1)
     {
         is_ready = false;
@@ -110,8 +72,5 @@ void main(void)
         while(!is_ready){};
 		k_sleep(K_MSEC(1000));
     }
-
-
-
 
 }
